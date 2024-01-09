d455eecdfd0016b7a2e0598ccf1dc3f7ae3a7776
AmitSinghDeveloper <amitsingh63051@gmail.com>
2024-01-10 03:24:55 +0530

    added all files

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+function cartBtn(){
+  document.querySelector('#js-cart-items-container').classList.toggle('active');
+  console.log('amit');
+};
+
+function loginBtn(){
+  document.querySelector('#js-login-container').classList.toggle('active');
+};
+
+
+function loginBackBtn(){
+  document.querySelector('#js-login-container').classList.remove('active')
+
+}
