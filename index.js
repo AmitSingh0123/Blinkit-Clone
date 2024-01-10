acacd67027edfb0852554cc756947808d671f7c3
AmitSinghDeveloper <amitsingh63051@gmail.com>
2024-01-10 22:34:21 +0530

    added Product section

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 
+onLoad();
+function onLoad(){
+
+};
 
 function cartBtn(){
   document.querySelector('#js-cart-items-container').classList.toggle('active');
@@ -20,3 +24,36 @@ function addressBtn(){
 function addressRemoveBtn(){
   document.querySelector('#js-address-btn').classList.remove('active')
 };
+
+// Product section
+
+// function genrateProductDataHtml(){
+//   for (let i = 0; i < productData.length; i++){
+//     let Product = document.querySelector('#js-Product-section');
+    
+//     Product.innerHtml =  
+//   };
+
+// };
+function showDisplayProduct(){
+  let productContainer = document.querySelector("#js-Product-section");
+  let newHtml = "";
+  for (let i = 0; i < productData.length; i++) {
+    let {img_src} = productData[i]
+    
+
+
+    newHtml += `
+    <div>
+    <a href="#"><img src="${img_src}" alt=""></a>
+    
+    </div>
+    
+    `;
+  }
+  productContainer.innerHTML = newHtml;
+}
+
+showDisplayProduct();
+
+console.log(productData);
